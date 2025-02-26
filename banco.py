53bf56e2b819e38531e360217b45c317fbd38f1e
KelveNunes <kelvenunes123@gmail.com>
2025-02-26 12:41:10 -0300

    criando funcoes para cadastar usuarios e novas contas

--- banco.py
+++ banco.py
@@ -7,7 +7,7 @@ limite_saque = 0
 limite_valor_saque = 0
 saques = []
 
-def deposito(valor):
+def deposito(valor,cpf,numero_conta):
     global conta
     if valor <= 0:
         print('Você não pode depositar valores iguais ou menores que 0!!')
@@ -18,7 +18,7 @@ def deposito(valor):
     return conta
         
 
-def saque(valor_saque):
+def saque(valor_saque,cpf,numero_conta):
 
     global conta
     global limite_saque
@@ -45,7 +45,7 @@ def saque(valor_saque):
     return limite_saque,conta,limite_valor_saque
     
    
-def estrato():
+def extrato(cpf,numero_conta):
     print(f'Saldo: R${conta:.2f}')
     print('Depósitos:')
     for deposito in depositos:
@@ -54,21 +54,57 @@ def estrato():
     for saque in saques:
         print(f'R${saque:.2f}')
 
-def exibir_menu():
+def exibir_menu_conta():
     return int(input( '''
         escolha umas das opções da conta:
         1 - depositar
         2 - sacar
         3 - extrato
+        4 - criar usuario
+        5 - criar conta
         0 - parar operação
     '''))
+
+lista_user = {}
+def new_user():
+
+    print('digite as informações de usuario!\n' )
+    nome = input('nome: \n' )
+    data_nascimento = input('data de nascimento: \n' )
+    cpf = int(input('CPF: \n' ))
+    endereco = input('endereço: \n' )
+
+    new_user = {
+        "nome":nome,
+        "data_nascimento":data_nascimento,
+        "endereco":endereco,
+    }
+    lista_user[cpf] = new_user
+    print('Usuário cadastrado com sucesso!')
+
+contas = {}
+def nova_conta_corrente(cpf):
+
+    if cpf in lista_user:
     
+        numero_conta = len(contas)+1
+        agencia = '0001'
+        nome = lista_user[cpf]["nome"]
+
+        new_acount = {
+           "agencia": agencia,
+           "nome": nome,
+        }
+        contas[numero_conta] = new_acount
+        print(f'Conta corrente {numero_conta} criada com sucesso para o CPF {cpf}.')
+    else:
+        print('Você precisa cadastrar um usuario antes')
 
 
 def main():
-
+    
     while True:
-        opcao = exibir_menu()
+        opcao = exibir_menu_conta()
 
         if opcao == 1:
             valor = float(input('Quanto você deseja depositar? '))
@@ -77,7 +113,12 @@ def main():
             valor = float(input('Quanto você deseja sacar? '))
             saque(valor)
         elif opcao == 3:
-            estrato()
+            extrato()
+        elif opcao == 4:
+            new_user()
+        elif opcao == 5:
+            cpf = int(input('digite o cpf para criar uma nova conta: '))
+            nova_conta_corrente(cpf)
         elif opcao == 0:
             print('finalizado.')
             break
