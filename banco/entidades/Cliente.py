6fd6e7d7c40a657935586e648f77269777f1119a
KelveNunes <kelvenunes123@gmail.com>
2025-03-07 12:44:14 -0300

    criando a classe conta

--- banco/entidades/Cliente.py
+++ banco/entidades/Cliente.py
@@ -4,13 +4,15 @@ from interfaces import Transacao
 
 class Cliente():
 
-    def __init__(self, endereco, contas):
+    def __init__(self, endereco, contas: list):
         
         self._endereco = endereco,
         self._contas = contas
 
     def realizar_transacao(self, conta:Conta, transacao: Transacao):
-        pass
+        
+        
+
 
     def adicionar_conta(self, conta:Conta):
         pass
--- banco/entidades/Conta.py
+++ banco/entidades/Conta.py
@@ -7,20 +7,44 @@ class Conta():
     
     def __init__(self, saldo, numero, agencia, cliente: Cliente, historico: Historico):
 
-        self._saldo = saldo,
-        self._numero = numero,
-        self._agencia = agencia,
-        self._cliente = cliente,
-        self._historico = historico,
-    
-    def saldo(self): float
         
-    def nova_conta(self): Conta
+        self._saldo = saldo
+        self._numero = numero
+        self._agencia = agencia
+        self._cliente = cliente
+        self._historico = historico
     
-    def sacar(self): bool
+    def saldo(self):
 
-    def depositar(self): bool
-
+        print(f"o seu saldo é {self._saldo}")
         
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+    
+        return cls(saldo=0, numero=numero, cliente=cliente, historico=Historico())
+    
+    def sacar(self, valor): 
+
+        if valor <= 0:
+            print('você não pode sacar valores menores que 0!!')
+            return False
+           
+        elif valor > self._saldo:
+            print(f'O valor do saque é maior que o saldo disponível em conta." O seu saldo é: {self._saldo}")')
+            return False
+            
+        else:
+            self._saldo -= valor
+            return True
+
+    def depositar(self, valor):
+
+        if valor <= 0:
+            print('Você não pode depositar valores iguais ou menores que 0!!')
+            return False
+        else:
+            self._saldo += valor
+            return True
+            
         
         
